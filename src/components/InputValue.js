119b31d9cfcd5f4ed6b3cea0d86cdec46ddc5159
woot <worrawutp@gmail.com>
2022-03-02 22:38:43 +0700

    Set interest to zero when user losing focus from its input box

--- src/components/InputValue.js
+++ src/components/InputValue.js
@@ -4,12 +4,19 @@ export default function InputValue(props) {
   const autoFocus = props.autoFocus != undefined ? true : false
   const step = props.step != undefined ? props.step : 'any' 
 
+  const handleBlur = (e) => {
+    if (props.handleBlur != undefined) {
+      props.handleBlur(e)
+    }
+  }
+
   return (
     <div className="flex gap-4 mb-4">
       <label htmlFor="period" className="w-32">{props.label}</label>
       <input type="number"
         className="px-4 py-2 border border-slate-400 flex-grow"
         value={props.value}
+        onBlur={e => handleBlur(e)}
         onChange={e => props.handleChange(e)}
         step={step}
         autoFocus={autoFocus}
