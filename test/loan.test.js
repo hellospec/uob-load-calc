c369fc31b186511c448d09a34c1a7794d6d4fb60
woot <worrawutp@gmail.com>
2022-03-02 11:02:48 +0700

    Add negative interest test and get fail result

--- test/loan.test.js
+++ test/loan.test.js
@@ -1,8 +1,26 @@
 import calculateInstallment from "../lib/loan";
 
+const data = {
+  amount: 100000,
+  interest: 2,
+  months: 48
+}
+
+const dataToParams = (d) => {
+  return Object.values(d)
+}
+
 test('calculate for good installment', () => {
-  const result = calculateInstallment(100000, 2, 48)
+  const params = dataToParams(data)
+  const result = calculateInstallment(...params)
+
   expect(result).toBe(2125)
   expect(typeof(result)).toBe("number")
 })
 
+test('negative interest', () => {
+  const params = dataToParams({ ...data, interest: -1 })
+  const result = calculateInstallment(...params) 
+  
+  expect(result).toBe(0)
+})
